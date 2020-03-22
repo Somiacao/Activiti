74adfc98edfcb8d74682c476ad8f5a94bcadc0aa
曹红梅 <18210240048@fudan.edu.cn>
2020-03-22 15:19:20 +0800

    activiti

---
+++ src/main/java/com/chm/demo/ActivitiWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chm.demo;
+
+import org.activiti.spring.boot.SecurityAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class ActivitiWebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiWebDemoApplication.class, args);
+    }
+}
